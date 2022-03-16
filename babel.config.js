8c9f61294207b0ed3b8a5198ebbe5e25962cd753
vinicius.agile <vinicius.agile@fleetcor.com.br>
2022-03-16 15:03:44 -0300

    Create and settings project

---
+++ babel.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ["module-resolver", {
+      root: ["./"],
+      alias: {
+        '^~(.+)': './src/\\1',
+      },
+      extensions: [
+        '.ios.js',
+        '.android.js',
+        '.js',
+        '.jsx',
+        '.json',
+        '.tsx',
+        '.ts',
+        '.native.js',
+        '.svg'
+      ],
+    }],
+  ]
+};
